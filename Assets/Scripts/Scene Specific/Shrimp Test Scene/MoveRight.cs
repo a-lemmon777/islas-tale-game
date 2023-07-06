eabe72b7f0cd5eb02e8c22909684c87c12d3d9a4
Song Yang <songyang.dev@gmail.com>
2023-07-06 18:33:49 -0400

    Properly setting up VSCode for Unity

---
+++ Assets/Scripts/Scene Specific/Shrimp Test Scene/MoveRight.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRight : MonoBehaviour
+{
+    [Tooltip("Speed in units per frame")]
+    public float Speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
