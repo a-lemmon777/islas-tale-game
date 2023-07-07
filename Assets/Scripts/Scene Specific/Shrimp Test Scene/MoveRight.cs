9575c88bc7edfd23977b25b9468dbb4475041724
Song Yang <songyang.dev@gmail.com>
2023-07-06 20:23:17 -0400

    Implement shrimp collision

--- Assets/Scripts/Scene Specific/Shrimp Test Scene/MoveRight.cs
+++ Assets/Scripts/Scene Specific/Shrimp Test Scene/MoveRight.cs
@@ -2,20 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class MoveRight : MonoBehaviour
 {
-    [Tooltip("Speed in units per frame")]
+    [Tooltip("Speed in units per second")]
     public float Speed;
+
+    /// <summary>
+    /// Rigidbody without gravity
+    /// </summary>
+    private Rigidbody2D _rigidbody2D;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        this._rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        this._rigidbody2D.MovePosition(
+            (Vector2)this.transform.position
+            + Vector2.right * this.Speed * Time.deltaTime
+        );
     }
 }
