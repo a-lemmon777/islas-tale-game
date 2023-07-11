9aa1e211ac9adcf5d2ac05a6b7a97c8e7db01b85
Song Yang <songyang.dev@gmail.com>
2023-07-11 18:19:23 -0400

    Simple rotation flipping

--- Assets/Scripts/Enemies/Shrimp/ShrimpMover.cs
+++ Assets/Scripts/Enemies/Shrimp/ShrimpMover.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,9 +9,6 @@ public class ShrimpMover : MonoBehaviour
     [Tooltip("Destination position to move to")]
     public Transform Destination;
 
-    [Tooltip("By default, the sprite looks towards +x. This vector will rotate the sprite to face toward the destination.")]
-    public Vector2 FacingTowards;
-
     [Tooltip("Speed in units per second")]
     public float Speed = 5;
 
@@ -20,17 +18,12 @@ public class ShrimpMover : MonoBehaviour
     void Start()
     {
         this._rigidbody2D = GetComponent<Rigidbody2D>();
-
-        // Rotate the image according to the facing vector
-        this.FacingTowards = (Vector2)Destination.position - _rigidbody2D.position;
-        this.FacingTowards.Normalize();
-        this.transform.Rotate(Vector3.forward * Vector2.Angle(Vector2.right, this.FacingTowards));
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        var distanceLeft = Vector2.Distance(Destination.position, _rigidbody2D.position);
+        float distanceLeft = Vector2.Distance(Destination.position, _rigidbody2D.position);
 
         // the shrimp arrived
         if (distanceLeft <= 0.05f) // proximity threshold, 0.05f makes the shrimp stop on the target
@@ -42,5 +35,9 @@ public class ShrimpMover : MonoBehaviour
         _rigidbody2D.velocity =
             ((Vector2)Destination.position - _rigidbody2D.position).normalized * Speed
         ;
+
+        // flip the image
+        if (_rigidbody2D.velocity.x < 0)
+            this.transform.localScale = new Vector3(-1, 1, 1);
     }
 }
