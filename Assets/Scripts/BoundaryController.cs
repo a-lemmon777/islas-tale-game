edb30676201f9057bf65794ccc728584e26db751
Aaron Lemmon <a.lemmon777@gmail.com>
2023-07-12 21:08:20 -0500

    Improved the movement code so the player won't slow down on the edges of the screen.

---
+++ Assets/Scripts/BoundaryController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class BoundaryController : MonoBehaviour
+{
+
+    private Collider2D _collider;
+    public const int SCREEN_WIDTH = 32;
+    public const int SCREEN_HEIGHT = 18;
+    public const int SCREEN_MIN_X = -(SCREEN_WIDTH / 2);
+    public const int SCREEN_MAX_X = SCREEN_WIDTH / 2;
+    public const int SCREEN_MIN_Y = -(SCREEN_HEIGHT / 2);
+    public const int SCREEN_MAX_Y = SCREEN_HEIGHT / 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _collider = GetComponent<Collider2D>();
+    }
+
+    void LateUpdate()
+    {
+        float halfColliderWidth = _collider.bounds.extents.x;
+        float halfColliderHeight = _collider.bounds.extents.y;
+        Vector2 clampedPosition = transform.position;
+        clampedPosition.x = Mathf.Clamp(clampedPosition.x, SCREEN_MIN_X + halfColliderWidth, SCREEN_MAX_X - halfColliderWidth);
+        clampedPosition.y = Mathf.Clamp(clampedPosition.y, SCREEN_MIN_Y + halfColliderHeight, SCREEN_MAX_Y - halfColliderHeight);
+        transform.position = clampedPosition;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,39 +1,92 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Threading;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
 public class PlayerCharacterScript : MonoBehaviour
 {
     [Tooltip("Speed in units per second")]
     public float Speed;
 
-    /// <summary>
-    /// Rigidbody without gravity
-    /// </summary>
     private Rigidbody2D _rigidbody2D;
+    private Collider2D _collider;
+    private float _horizontalInput = 0f;
+    private float _verticalInput = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        this._rigidbody2D = GetComponent<Rigidbody2D>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<Collider2D>();
     }
 
     // FixedUpdate is called once per physics frame
     void FixedUpdate()
     {
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        float verticalInput = Input.GetAxisRaw("Vertical");
+        _horizontalInput = Input.GetAxisRaw("Horizontal");
+        _verticalInput = Input.GetAxisRaw("Vertical");
 
-        Vector2 horizontalInputVector = new Vector2(horizontalInput, 0).normalized;
-        Vector2 verticalInputVector = new Vector2(0, verticalInput).normalized;
-        Vector2 direction = (horizontalInputVector + verticalInputVector).normalized;
+        float halfColliderWidth = _collider.bounds.extents.x;
+        float halfColliderHeight = _collider.bounds.extents.y;
 
-        Vector2 movement = direction * Speed * Time.deltaTime;
-        Vector2 position = (Vector2) this.transform.position;
-        position += movement;
+        float lowerBoundX = BoundaryController.SCREEN_MIN_X + halfColliderWidth;
+        float upperBoundX = BoundaryController.SCREEN_MAX_X - halfColliderWidth;
+        float lowerBoundY = BoundaryController.SCREEN_MIN_Y + halfColliderHeight;
+        float upperBoundY = BoundaryController.SCREEN_MAX_Y - halfColliderHeight;
+
+        float remainingDistanceX = _horizontalInput > 0 ? upperBoundX - transform.position.x : lowerBoundX - transform.position.x;
+        float remainingDistanceY = _verticalInput > 0 ? upperBoundY - transform.position.y : lowerBoundY - transform.position.y;
+
+        Vector2 movement = Vector2.zero;
+        float movementMagnitude = Speed * Time.deltaTime;
+
+        // Used for Pythagorean Theorem
+        float movementMagnitudeSquared = Mathf.Pow(movementMagnitude, 2);
+
+        // The case when the player inputs diagonal movement.
+        if (Mathf.Abs(_horizontalInput) > 0 && Mathf.Abs(_verticalInput) > 0)
+        {
+            // The magnitude of movement per axis if the full movement is split equally between both dimensions.
+            float perAxisEqualMovement = Mathf.Sqrt(movementMagnitudeSquared / 2);
+
+            // The case when the player is close to the left or right edge.
+            if (Mathf.Abs(remainingDistanceX) < perAxisEqualMovement)
+            {
+                movement.x = remainingDistanceX;
+                float availableYMagnitude = Mathf.Sqrt(movementMagnitudeSquared - Mathf.Pow(movement.x, 2));
+                float movementY = Mathf.Clamp(remainingDistanceY, -availableYMagnitude, availableYMagnitude);
+                movement.y = movementY;
+            }
+            // The case when the player is close to the upper or lower edge.
+            else if (Mathf.Abs(remainingDistanceY) < perAxisEqualMovement)
+            {
+                movement.y = remainingDistanceY;
+                float availableXMagnitude = Mathf.Sqrt(movementMagnitudeSquared - Mathf.Pow(movement.y, 2));
+                float movementX = Mathf.Clamp(remainingDistanceX, -availableXMagnitude, availableXMagnitude);
+                movement.x = movementX;
+            }
+            // The case when the player is not close to any edge.
+            else
+            {
+                movement.x = _horizontalInput > 0 ? perAxisEqualMovement : -perAxisEqualMovement;
+                movement.y = _verticalInput > 0 ? perAxisEqualMovement : -perAxisEqualMovement;
+            }
+        }
+        // The case when the player inputs only horizontal movement.
+        else if (Mathf.Abs(_horizontalInput) > 0)
+        {
+            movement.x = Mathf.Clamp(remainingDistanceX, -movementMagnitude, movementMagnitude);
+        }
+        // The case when the player inputs only vertical movement.
+        else if (Mathf.Abs(_verticalInput) > 0)
+        {
+            movement.y = Mathf.Clamp(remainingDistanceY, -movementMagnitude, movementMagnitude);
+        }
+
+        Vector2 position = (Vector2)transform.position + movement;
 
         _rigidbody2D.MovePosition(position);
     }
