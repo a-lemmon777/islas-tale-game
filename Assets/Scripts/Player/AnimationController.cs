0e535e13d968658ec4cad05a1738cb2b1d97d6b7
Song Yang <songyang.dev@gmail.com>
2023-07-14 19:36:46 -0400

    Integrate mermaid movement animations

--- Assets/Scripts/Player/AnimationController.cs
+++ Assets/Scripts/Player/AnimationController.cs
@@ -16,6 +16,10 @@ public class AnimationController : MonoBehaviour
         _animator = GetComponent<Animator>();
     }
 
+    /// <summary>
+    /// Triggers the appropriate animation parameters in the state machine
+    /// </summary>
+    /// <param name="normalizedInput">An instantaneous frame movement delta</param>
     public void HandleMovement(Vector2 normalizedInput)
     {
         _animator.SetFloat("Horizontal Direction", normalizedInput.x);
