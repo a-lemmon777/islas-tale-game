39a0200fe8bc18f0acfca776cb78b3bae86a932e
Aaron Lemmon <a.lemmon777@gmail.com>
2023-07-13 19:41:14 -0500

    Added speed and spin to the starfish script.

--- Assets/Scripts/Player/StarfishController.cs
+++ Assets/Scripts/Player/StarfishController.cs
@@ -1,18 +1,25 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class StarfishController : MonoBehaviour
 {
+    [SerializeField]
+    [Tooltip("Speed in units per second")]
+    private float Speed = 20f;
+
+    [SerializeField]
+    [Tooltip("Rate of spin in degrees per second")]
+    private float AngularVelocity = -720f;
+
+    Rigidbody2D _rigidbody2D;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _rigidbody2D.velocity = Vector2.right * Speed;
+        _rigidbody2D.angularVelocity = AngularVelocity;
     }
 }
