fffb159819e6dd68c5d8f57cbee1f2b57af3909b
Song Yang <songyang.dev@gmail.com>
2023-07-16 21:37:29 -0400

    Create abstract health and mermaid health

---
+++ Assets/Scripts/CharacterHealth.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Parent class that holds basic features of HP
+/// </summary>
+abstract public class CharacterHealth : MonoBehaviour
+{
+    [Tooltip("Max health of this game character")]
+    public int MaxHealth;
+
+    [Tooltip("Current health points of the character")]
+    public int Health;
+
+    /// <summary>
+    /// Decrements the health and triggers death if appropriate
+    /// </summary>
+    /// <param name="damageValue">How much take to take</param>
+    public void TakeDamage(int damageValue)
+    {
+        Health -= damageValue;
+
+        if (Health <= 0) Die();
+    }
+
+    /// <summary>
+    /// Increments the health up to max health
+    /// </summary>
+    /// <param name="healValue"></param>
+    public void Heal(int healValue)
+    {
+        Health += healValue;
+
+        Health = Mathf.Max(Health, MaxHealth);
+    }
+
+    /// <summary>
+    /// Called by TakeDamage when Health reaches 0 or less
+    /// </summary>
+    abstract public void Die();
+}
