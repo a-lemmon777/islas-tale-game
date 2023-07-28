1288ca7cd271e34fbd873b09de4fb27b4e892e1f
Song Yang <songyang.dev@gmail.com>
2023-07-28 16:38:20 -0400

    Add enemy spawner and waves

---
+++ Assets/Scripts/Level Management/EnemySpawner.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [Tooltip("Scriptable object of level events")]
+    public LevelEvents LevelEvents;
+
+    [Tooltip("List of enemy waves")]
+    public List<WaveManager> WaveList;
+
+    [Tooltip("The current wave number")]
+    public int CurrentWave = 0;
+
+    [Tooltip("When the current wave ends")]
+    public UnityEvent WaveCompleted;
+
+    private void Awake()
+    {
+        WaveCompleted.AddListener(() =>
+        {
+            if (CurrentWave == WaveList.Count - 1)
+            {
+                LevelEvents.Victory.Invoke();
+                return;
+            }
+
+            CurrentWave++;
+            WaveList[CurrentWave].Activate.Invoke();
+        });
+    }
+
+    void OnDisable()
+    {
+        WaveCompleted.RemoveAllListeners();
+    }
+
+    private void Start()
+    {
+        WaveList[0].Activate.Invoke();
+    }
+}
